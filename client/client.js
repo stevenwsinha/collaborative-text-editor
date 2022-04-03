6cd6844196191d770f1d6af30a3ab8c3c5e09a3a
Steven Sinha <stevensinha329@gmail.com>
2022-04-03 11:13:46 -0400

    implement document loading on initial connection

--- client/client.js
+++ client/client.js
@@ -1,41 +1,52 @@
-var delta_queue = []
 var connectionId
 
 var quill = new Quill('#doc-container', {
     theme: 'snow'
 });
 
-quill.on('text-change', function(delta, oldDelta, source) {
-    console.log(delta)
-    console.log(delta.ops)
-    delta_queue.push(delta)
+quill.on('text-change', async function(delta, oldDelta, source) {
+    let opsURL = "/op/" + connectionId
+    let payload = {payload: delta}
+    let response = await fetch(opsURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload)                                            
+    })
 })
 
 
 // setInterval(sendQueue, 3000)
 
-async function sendQueue() {
-    /* this func is async to ensure it doesn't block
-     * user input from being added to the queue
-     * we want to avoid async madness where delta_queue is growing as we
-     * consolidate it, so we only iterate through its current length
-     * this works because the queue is monotomically non-decreasing */
-    let length = delta_queue.length   
+// async function sendQueue() {
+//     /* this func is async to ensure it doesn't block
+//      * user input from being added to the queue
+//      * we want to avoid async madness where delta_queue is growing as we
+//      * consolidate it, so we only iterate through its current length
+//      * this works because the queue is monotomically non-decreasing */
+//     let length = delta_queue.length   
 
-    for(let i = 0; i < length; i++) {
-        let oplist = delta_queue[i]
-    }
-}
+//     for(let i = 0; i < length; i++) {
+//         let oplist = delta_queue[i]
+//     }
+// }
 
 /*
  * establish the connection
  */
 
-window.onload = function() {
-    connectionId = Math.floor(Math.random() * 10000000);
-    console.log(connectionId)
-    let connectionURL = "/connect:" + connectionId.toString()
+window.onload = async function() {
+    connectionId = Math.floor(Math.random() * 10000000).toString();
+    let connectionURL = "/connect/" + connectionId
+    let response = await fetch(connectionURL)
+    let responseJson = await response.json()
+    console.log(responseJson)
+    updateDoc(responseJson.payload)
+}
 
-    let response = fetch(connectionURL)
-    console.log(response)
+function updateDoc(doc) {
+    for (oplist of doc) {
+        quill.updateContents(oplist, 'api')
+    }
 }
--- web-server/index.js
+++ web-server/index.js
@@ -1,16 +1,60 @@
+/*
+ *  IMPORT THE NECESSARY MODULES AND SET GLOBAL CONSTANTS
+ */ 
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// creat global doc
+let doc = []
+
+/*
+ *  SET UP EXPRESS MIDDLEWARE/STATIC CONTENT SERVING
+ */
 app.use(express.static("../client"))
 app.use(bodyParser.json());
 
-app.get('/connect:id', function(req, res){
-    connectionId = req.params.id.substring(1, req.params.id.length)
-    console.log(connectionId)
+/*
+ *  SET UP EXPRESS ROUTING
+ */
+app.get('/connect/:id', function(req, res) {
+    connectionId = req.params.id
+    res.json({
+        payload: doc
+    }).send()
+})
 
-    res.send("OK")
+app.post('/op/:id', function(req, res) {
+    connectionId = req.params.id
+    oplist = req.body.payload.ops
+    doc.push(oplist)
+    res.end()
+})
+
+app.get('/doc/:id', function(req, res) {
+    connectionId = req.params.id
+    doc = generateDoc();
+    res.write(doc).end()
 })
 
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`))
+
+function generateDoc() {
+    html = "<html> <body> <p>"
+
+    length = doc.length
+    console.log(doc.length)
+    for (let i = 0; i < length; i++) {
+        oplist = doc[i]
+        if(oplist.length > 1){
+            html += oplist[1].insert
+        }
+        else{
+            html += oplist[0].insert
+        }
+    }
+
+    html += "</p> </body> </html>"
+    return html
+}
