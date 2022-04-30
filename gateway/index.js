b9105cdb701e2439bea02211a663c1ab6c439d9b
Steven Sinha <stevensinha329@gmail.com>
2022-04-30 17:16:54 -0400

    add basic api gateway

---
+++ gateway/index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const httpProxy = require('express-http-proxy')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cookieParser())
+const PORT = process.env.PORT || 3000
+
+/*
+ *  EVERYTHING NEEDS THE CSE356 HEADER
+ */
+function setHeader(req, res, next) {
+    res.set('X-CSE356', '620bd941dd38a6610218bb1b')
+    next()
+}
+app.use(setHeader)
+
+
+/*
+ *  AUTH FUNCTION
+ */
+function auth(req, res, next) {
+    if(!req.cookies['id']) {
+        return res.json({error: true, message: "received api call without authorization"})
+    }
+    next()
+}
+
+/*
+ *  DEFINE ALL OUR PROXIES
+ */
+const collectionsProxy = httpProxy('localhost:4000')
+//const usersProxy = httpProxy('localhost:5000')
+//const mediaProxy = httpProxy('localhost:6000')
+//const docProxy = httpProxy('localhost:7000')
+
+app.all("/users/*", async function (req, res) {
+    usersProxy(req, res)
+})
+
+app.post("/collection/create", auth, async function (req, res) {
+    collectionsProxy(req, res)
+})
+
+app.all("/media/*", auth, async function (req, res) {
+    mediaProxy(req, res)
+})
+
+app.all("/doc/*", auth, async function (req, res) {
+    docProxy(req, res)
+})
+
+app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`))
