22465fe5ac9b7bf8b8fcb26d2933385947272e2c
Steven Sinha <stevensinha329@gmail.com>
2022-05-08 03:40:56 -0400

    change index service to fetch doc content

--- index/index.js
+++ index/index.js
@@ -1,11 +1,30 @@
 const bodyParser = require('body-parser')
 var cron = require('node-cron');
+const sharedbClient = require('sharedb/lib/client')
+const richText = require('rich-text');
+const WebSocket = require('ws');
 const axios = require('axios')
 const axios_es = axios.create({
     baseURL: 'http://localhost:9000',
     timeout: 5000,
 })
 
+const axios_collection = axios.create({
+    baseURL: 'http://localhost:4000',
+    timeout: 5000,
+})
+
+/*
+ *  CREATE CONNECTION TO SHAREDB SERVER
+ */
+sharedbClient.types.register(richText.type);
+let ws = new WebSocket('ws://localhost:8080');
+let connection = new sharedbClient.Connection(ws);
+console.log("Connected to sharedb server")
+
+/*
+ *  SETUP CONNECTIONS TO DOC SERVERS
+ */
 doc_servers = ['localhost:7000', 'localhost:7001']
 axios_instances = []
 index = 0
@@ -25,6 +44,34 @@ async function requestChangedDocs() {
     
     let response = await axios_instances[index].get("/doc/changed")    
     ids = response.data.docids
+
+    let namePairs = await axios_collection.post("/collection/changed", {
+                                                                            docids: ids
+                                                                        })    
+
+
+    for (let i = 0; i < changed.length; i++) {
+        docid = ids[i]
+        doc = connection.get('docs', docid)
+        doc.fetch(async () => {
+            if(doc._type === null) {
+                return
+            }
+    
+            // index the doc
+            content = doc.data.ops
+            console.log(content)
+
+            await client.index({
+                index: 'docs',
+                id: docid,
+                document: {
+                    title: namePairs[i],
+                    body: "full document text"
+                }
+            })
+        })    
+    }
 
     index++
     index%=doc_servers.length
